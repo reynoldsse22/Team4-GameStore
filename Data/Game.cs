9918446ca76a3dbec11746777d10c803e05602d1
reynoldsse22 <63010181+reynoldsse22@users.noreply.github.com>
2021-11-22 11:02:06 -0500

    Major facelift to Used Games page
    Added Quantity text field and fixed layout of games. Added styling to text and added a markdown price value.

--- Data/Game.cs
+++ Data/Game.cs
@@ -3,7 +3,19 @@ public class Game
     public int ID {get;set;}
     public string Name{get; set;}
     public float Price{get;set;}
+
+    public float OriginalPrice{get; set;}
     public string ImageLink{get; set;}
     public string Description{get;set;}
 
+    public Console Console{get;set;}
+
+}
+
+public enum Console{
+    PC,
+    Playstation,
+    XBOX,
+    Switch,
+    Wii
 }
--- Data/GameService.cs
+++ Data/GameService.cs
@@ -13,26 +13,42 @@ namespace BlazorApp.Data
                 new()
                 {
                     ID = 1,
-                    Name = "Rockman", 
+                    Name = "Grand Theft Auto V", 
                     Price = 29.99f,
-                    ImageLink = "Images/deeznutssss.webp",
-                    Description = "Technology."
+                    OriginalPrice = 34.99f,
+                    ImageLink = "Images/GTAV.jpg",
+                    Description = "Grand Theft Auto V is an action-adventure game played from either a third-person or first-person perspective. Players complete missions—linear scenarios with set objectives—to progress through the story. Outside of the missions, players may freely roam the open world.",
+                    Console = Console.PC
                 },
                 new()
                 {
                     ID = 2,
                     Name = "Mario Pratt", 
                     Price = 19.99f,
-                    ImageLink = "Images/Mario.jpg",
-                    Description = "A tutorial book for Blazor WASM technology."
+                    OriginalPrice = 24.99f,
+                    ImageLink = "Images/SuperMario.png",
+                    Description = "In the game, Mario must race through the Mushroom Kingdom and save Princess Toadstool (later Princess Peach) from Bowser. Mario jumps, runs, and walks across each level. The worlds are full of enemies and platforms, and open holes.",
+                    Console = Console.Playstation
                 },
                 new()
                 {
                     ID = 3,
                     Name = "Skyrim 90th edition", 
                     Price = 14.99f,
+                    OriginalPrice = 34.99f,
                     ImageLink = "Images/skyrim.png",
-                    Description = "Follow the journey of a programmer."
+                    Description = "The Elder Scrolls V: Skyrim is an action role-playing game, playable from either a first or third-person perspective. The player may freely roam over the land of Skyrim—an open world environment consisting of wilderness expanses, dungeons, caves, cities, towns, fortresses, and villages.",
+                    Console = Console.PC
+                },
+                new()
+                {
+                    ID = 4,
+                    Name = "Call of Duty", 
+                    Price = 17.99f,
+                    OriginalPrice = 31.49f,
+                    ImageLink = "Images/COD.jpg",
+                    Description = "The game simulates the infantry and combined arms warfare of World War II",
+                    Console = Console.XBOX
                 }
             };
         }
