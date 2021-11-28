31fafe3dcf155f24b864649f5cefe9dd70327e88
reynoldsse22 <63010181+reynoldsse22@users.noreply.github.com>
2021-11-28 18:44:29 -0500

    Added small checkout page and unique reviews

--- obj/Debug/net5.0/RazorDeclaration/Pages/Checkout.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Checkout.razor.g.cs
@@ -13,64 +13,71 @@ namespace BlazorApp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\cocar\Documents\GitHub\Team4-GameStore\_Imports.razor"
+#line 1 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\cocar\Documents\GitHub\Team4-GameStore\_Imports.razor"
+#line 2 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\cocar\Documents\GitHub\Team4-GameStore\_Imports.razor"
+#line 3 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\cocar\Documents\GitHub\Team4-GameStore\_Imports.razor"
+#line 4 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\cocar\Documents\GitHub\Team4-GameStore\_Imports.razor"
+#line 5 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\cocar\Documents\GitHub\Team4-GameStore\_Imports.razor"
+#line 6 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\cocar\Documents\GitHub\Team4-GameStore\_Imports.razor"
+#line 7 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\cocar\Documents\GitHub\Team4-GameStore\_Imports.razor"
+#line 8 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\cocar\Documents\GitHub\Team4-GameStore\_Imports.razor"
+#line 9 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\_Imports.razor"
 using BlazorApp;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 4 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\Pages\Checkout.razor"
+using BlazorApp.Shared;
 
 #line default
 #line hidden
@@ -83,6 +90,34 @@ using BlazorApp;
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 20 "C:\Users\samre\OneDrive\Desktop\Team4-GameStore\Team4-GameStore\Pages\Checkout.razor"
+ 
+    List<Game> Games = new List<Game>();
+    protected override async Task OnInitializedAsync()
+    {
+        await RefreshCart();
+    }
+
+    private async Task RefreshCart()
+    {
+        Games = await service.GetCart();
+        foreach(Game game in Games)
+        {
+            await service.DeleteGameAsync(game, game.Quantity);
+        }  
+    }  
+
+    private void BackToHome()
+    {
+        UriHelper.NavigateTo("/", forceLoad:true);
+    }
+
+#line default
+#line hidden
+#nullable disable
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private NavigationManager UriHelper { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private CartDbService service { get; set; }
     }
 }
 #pragma warning restore 1591
